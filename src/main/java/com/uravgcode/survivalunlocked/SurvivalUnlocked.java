a44880a0e8d5fb795aa03167896deb70b17af693
UrAvgCode <dev@uravgcode.com>
2025-09-30 22:42:22 +0200

    automatically update config

--- src/main/java/com/uravgcode/survivalunlocked/SurvivalUnlocked.java
+++ src/main/java/com/uravgcode/survivalunlocked/SurvivalUnlocked.java
@@ -5,11 +5,15 @@ import com.uravgcode.survivalunlocked.annotation.Feature;
 import com.uravgcode.survivalunlocked.feature.FeatureList;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.Objects;
 
 public final class SurvivalUnlocked extends JavaPlugin {
 
@@ -20,10 +24,23 @@ public final class SurvivalUnlocked extends JavaPlugin {
             saveResource("heads.yml", false);
         }
 
-        var config = getConfig();
         var logger = getComponentLogger();
-        var pluginManager = getServer().getPluginManager();
+        var pluginVersion = getPluginMeta().getVersion();
+        var configVersion = getConfig().getString("config.version", "0.0.0");
+
+        if (!pluginVersion.equals(configVersion)) {
+            try {
+                updateConfig();
+                reloadConfig();
+                logger.info("updating config to version {}", pluginVersion);
+            } catch (IOException exception) {
+                logger.warn("failed to update config: {}", exception.getMessage());
+            }
+        }
+
+        var config = getConfig();
         var features = FeatureList.getFeatures(this);
+        var pluginManager = getServer().getPluginManager();
 
         for (var feature : features) {
             var annotation = feature.getClass().getAnnotation(Feature.class);
@@ -31,7 +48,7 @@ public final class SurvivalUnlocked extends JavaPlugin {
 
             var path = annotation.name();
             if (config.getBoolean("features." + path + ".enabled", false)) {
-                injectConfigValues(feature, config, this);
+                injectConfigValues(feature);
                 pluginManager.registerEvents(feature, this);
                 logger.info(Component.text(path + " enabled", NamedTextColor.GREEN));
             } else {
@@ -40,10 +57,28 @@ public final class SurvivalUnlocked extends JavaPlugin {
         }
     }
 
-    public static void injectConfigValues(Listener feature, FileConfiguration config, JavaPlugin plugin) {
+    private void updateConfig() throws IOException {
+        var currentConfig = getConfig();
+        var defaultConfig = YamlConfiguration.loadConfiguration(
+            new InputStreamReader(Objects.requireNonNull(getResource("config.yml")))
+        );
+
+        for (String key : defaultConfig.getKeys(true)) {
+            if (key.startsWith("config")) continue;
+            if (currentConfig.contains(key)) {
+                defaultConfig.set(key, currentConfig.get(key));
+                defaultConfig.setComments(key, currentConfig.getComments(key));
+            }
+        }
+
+        defaultConfig.save(new File(getDataFolder(), "config.yml"));
+    }
+
+    private void injectConfigValues(Listener feature) {
         if (!feature.getClass().isAnnotationPresent(Feature.class)) return;
         var featurePath = feature.getClass().getAnnotation(Feature.class).name();
 
+        var config = getConfig();
         for (var field : feature.getClass().getDeclaredFields()) {
             if (!field.isAnnotationPresent(ConfigValue.class)) continue;
             var annotation = field.getAnnotation(ConfigValue.class);
@@ -55,7 +90,7 @@ public final class SurvivalUnlocked extends JavaPlugin {
                 var value = config.get(path);
                 field.set(feature, value);
             } catch (IllegalAccessException e) {
-                plugin.getLogger().warning("failed to inject config value for " + field.getName());
+                getLogger().warning("failed to inject config value for " + field.getName());
             }
         }
     }
